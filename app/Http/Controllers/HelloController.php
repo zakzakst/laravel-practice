9ca61d1bba6210ce736198840b5bf09d95289064
zakzakst <e1433104@gmail.com>
2022-03-13 14:13:07 +0900

    リスト2-11,12

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,35 +4,48 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+global $head, $style, $body, $end;
+
+$head = '<html><head>';
+$style = <<<EOF
+<style>
+body {
+  font-size: 16pt;
+  color: #999;
+}
+h1 {
+  font-size: 100pt;
+  text-align: right;
+  color: #eee;
+  margin: -40px 0 -50px 0;
+}
+</style>
+EOF;
+$body = '</head><body>';
+$end = '</body></html>';
+
+function tag($tag, $txt) {
+  return "<{$tag}>" . $txt . "</{$tag}>";
+}
+
 class HelloController extends Controller
 {
-  public function index($id='noname', $pass="unknown") {
-    return <<<EOF
-      <html>
-      <head>
-      <title>Hello</title>
-      <style>
-      body {
-        font-size: 16pt;
-        color: #999;
-      }
-      h1 {
-        font-size: 100pt;
-        text-align: right;
-        color: #eee;
-        margin: -40px 0 -50px 0;
-      }
-      </style>
-      </head>
-      <body>
-        <h1>Index</h1>
-        <p>これは、Helloコントローラーのindexアクションです。</p>
-        <ul>
-          <li>ID: {$id}</li>
-          <li>PASS: {$pass}</li>
-        </ul>
-      </body>
-      </html>
-    EOF;
+  public function index() {
+    global $head, $style, $body, $end;
+    $html = $head . tag('title', 'Hello/Index') .$style . $body
+      . tag('h1', 'Index')
+      . tag('p', 'this is Index page')
+      . '<a href="/hello/other">go to Other page</a>'
+      . $end;
+    return $html;
+  }
+
+  public function other() {
+    global $head, $style, $body, $end;
+    $html = $head . tag('title', 'Hello/Other') .$style . $body
+      . tag('h1', 'Other')
+      . tag('p', 'this is Other page')
+      . $end;
+    return $html;
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ Route::get('/', function () {
   return view('welcome');
 });
 
-Route::get('hello/{id?}/{pass?}', 'App\Http\Controllers\HelloController@index');
+Route::get('hello', 'App\Http\Controllers\HelloController@index');
+Route::get('hello/other', 'App\Http\Controllers\HelloController@other');
