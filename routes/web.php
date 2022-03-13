68421afd14cdbae4a90faefdc9b39b132c029898
zakzakst <e1433104@gmail.com>
2022-03-13 13:02:22 +0900

    リスト2-4

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,31 @@ Route::get('/', function () {
   return view('welcome');
 });
 
-Route::get('hello', function () {
-  return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {
+  font-size: 16pt;
+  color: #999;
+}
+h1 {
+  font-size: 100pt;
+  text-align: right;
+  color: #eee;
+  margin: -40px 0 -50px 0;
+}
+</style>
+</head>
+<body>
+  <h1>Hello</h1>
+  <p>This is sample page.</p>
+  <p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
+
+Route::get('hello', function () use ($html) {
+  return $html;
 });
