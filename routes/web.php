78e10692d09850726ca1645a4df8b464d3dbb981
zakzakst <e1433104@gmail.com>
2022-03-13 12:56:24 +0900

    リスト2-3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+Route::get('hello', function () {
+  return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
 });
