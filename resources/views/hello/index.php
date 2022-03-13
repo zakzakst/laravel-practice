104bb3aa38e5cd6ac74fbc16407ad43749e2b7ae
zakzakst <e1433104@gmail.com>
2022-03-13 16:20:26 +0900

    リスト3-1,2

---
+++ resources/views/hello/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+  <title>Hello/Index</title>
+  <style>
+  body {
+    font-size: 16pt;
+    color: #999;
+  }
+  h1 {
+    font-size: 100pt;
+    text-align: right;
+    color: #eee;
+    margin: -40px 0 -50px 0;
+  }
+  </style>
+</head>
+<body>
+  <h1>Index</h1>
+  <p>This is a sample page with php-template.</p>
+</body>
+</html>
