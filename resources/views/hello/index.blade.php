ed919d0ddc32dfaeaa88a9d3a98bb85780293c01
zakzakst <e1433104@gmail.com>
2022-03-15 03:02:55 +0900

    リスト3-24

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -18,15 +18,17 @@
   <h1>Blade/Index</h1>
   <p>&#064;forディレクティブの例</p>
   <ol>
-    @for ($i = 1; $i < 100; $i++)
-      @if ($i % 2 == 1)
-        @continue
-      @elseif ($i <= 10)
-        <li>No, {{$i}}</li>
-      @else
-        @break
+    @foreach ($data as $item)
+      @if ($loop->first)
+        <p>データ一覧</p>
+        <ul>
       @endif
-    @endfor
+        <li>No, {{$loop->iteration}}. {{$item}}</li>
+      @if ($loop->last)
+        </ul>
+        <p>ここまで</p>
+      @endif
+    @endforeach
   </ol>
 </body>
 </html>
