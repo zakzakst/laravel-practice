603de8672ce8222cc6ed9e02b9008270d6394fd7
zakzakst <e1433104@gmail.com>
2022-03-15 02:53:31 +0900

    リスト3-21,22

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
   public function index() {
-    return view('hello.index');
+    $data = ['one', 'two', 'three', 'four', 'five'];
+    return view('hello.index', ['data'=>$data]);
   }
 
   public function post(Request $request) {
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -16,15 +16,11 @@
 </head>
 <body>
   <h1>Blade/Index</h1>
-  @isset ($msg)
-  <p>こんにちは、{{ $msg }}さん。</p>
-  @else
-  <p>何か書いてください。</p>
-  @endisset
-  <form method="POST" action="/hello">
-    @csrf
-    <input type="text" name="msg">
-    <input type="submit">
-  </form>
+  <p>&#064;foreachディレクティブの例</p>
+  <ol>
+    @foreach($data as $item)
+    <li>{{$item}}</li>
+    @endforeach
+  </ol>
 </body>
 </html>
