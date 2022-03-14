637db135caae45b08a68c26c571bfd34d37b3c0b
zakzakst <e1433104@gmail.com>
2022-03-15 02:57:19 +0900

    リスト3-23

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -16,11 +16,17 @@
 </head>
 <body>
   <h1>Blade/Index</h1>
-  <p>&#064;foreachディレクティブの例</p>
+  <p>&#064;forディレクティブの例</p>
   <ol>
-    @foreach($data as $item)
-    <li>{{$item}}</li>
-    @endforeach
+    @for ($i = 1; $i < 100; $i++)
+      @if ($i % 2 == 1)
+        @continue
+      @elseif ($i <= 10)
+        <li>No, {{$i}}</li>
+      @else
+        @break
+      @endif
+    @endfor
   </ol>
 </body>
 </html>
