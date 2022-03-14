ffa3510e4dc1ac12fc541acf0fdcc7829e9af373
zakzakst <e1433104@gmail.com>
2022-03-15 04:45:03 +0900

    リスト3-31,32,33

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,18 +10,9 @@
 
 @section('content')
   <p>ここが本文のコンテンツです。</p>
-  <p>必要なだけ記述できます。</p>
-
-  <!-- @component('components.message')
-    @slot('msg__title')
-      CAUTION!
-    @endslot
-    @slot('msg__content')
-      これはメッセージの表示です。
-    @endslot
-  @endcomponent -->
-
-  @include('components.message', ['msg__title'=>'OK', 'msg__content'=>'サブビューです。'])
+  <ul>
+    @each('components.item', $data, 'item')
+  </ul>
 @endsection
 
 @section('footer')
