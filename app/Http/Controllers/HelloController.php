991060dff78ff9c96a8dc6b7d551292e3b8478ff
zakzakst <e1433104@gmail.com>
2022-03-15 02:48:29 +0900

    リスト3-19,20

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,10 +7,7 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
   public function index() {
-    $data = [
-      'msg' => '',
-    ];
-    return view('hello.index', $data);
+    return view('hello.index');
   }
 
   public function post(Request $request) {
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -16,11 +16,11 @@
 </head>
 <body>
   <h1>Blade/Index</h1>
-  @if ($msg != '')
-  <p>{{ $msg }}</p>
+  @isset ($msg)
+  <p>こんにちは、{{ $msg }}さん。</p>
   @else
   <p>何か書いてください。</p>
-  @endif
+  @endisset
   <form method="POST" action="/hello">
     @csrf
     <input type="text" name="msg">
