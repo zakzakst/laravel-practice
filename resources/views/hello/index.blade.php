49a4b60741cf1a665a5c3a625aedb9191e13e28d
zakzakst <e1433104@gmail.com>
2022-03-15 04:29:30 +0900

    リスト3-26,27

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,32 +1,18 @@
-<html>
-<head>
-  <title>Hello/Index</title>
-  <style>
-  body {
-    font-size: 16pt;
-    color: #999;
-  }
-  h1 {
-    font-size: 100pt;
-    text-align: right;
-    color: #eee;
-    margin: -40px 0 -50px 0;
-  }
-  </style>
-</head>
-<body>
-  <h1>Blade/Index</h1>
-  <p>&#064;whileディレクティブの例</p>
-  <ol>
-    @php
-      $counter = 0;
-    @endphp
-    @while ($counter < count($data))
-      <li>{{$data[$counter]}}</li>
-      @php
-        $counter++;
-      @endphp
-    @endwhile
-  </ol>
-</body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+  @parent
+  インデックスページ</li>
+  <li>テスト
+@endsection
+
+@section('content')
+  <p>ここが本文のコンテンツです。</p>
+  <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+  copyright 2020 tuyano.
+@endsection
---
+++ resources/views/layouts/helloapp.blade.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+  <title>@yield('title')</title>
+  <style>
+    body {font-size: 16pt; color: #999; margin: 5px;}
+    h1 {font-size: 50pt; text-align: right; color: #f6f6f6; margin: -20px 0 -30px 0; letter-spacing: -4pt}
+    ul {font-size: 12pt;}
+    hr {margin: 25px 100px; border-top: 1px dashed #ddd;}
+    .menutitle {font-size: 14pt; font-weight: bold; margin: 0;}
+    .content {margin: 10px;}
+    .footer {text-align: right; font-size: 10pt; margin: 10px; border-bottom: 1px solid #ccc; color: #ccc;}
+  </style>
+</head>
+<body>
+  <h1>@yield('title')</h1>
+  @section('menubar')
+  <h2 class="menutitle">※メニュー</h2>
+  <ul>
+    <li>@show</li>
+  </ul>
+  <hr size="1">
+  <div class="content">
+    @yield('content')
+  </div>
+  <div class="footer">
+    @yield('footer')
+  </div>
+</body>
+</html>
