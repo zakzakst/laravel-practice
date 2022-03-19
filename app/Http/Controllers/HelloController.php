a0d965c69b5fca825a459770c53fed9cc2d1152d
zakzakst <e1433104@gmail.com>
2022-03-19 23:34:37 +0900

    リスト4-26

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -22,11 +22,18 @@ class HelloController extends Controller
   }
 
   public function post(Request $request) {
-    $validator = Validator::make($request->all(), [
+    $rules = [
       'name' => 'required',
       'mail' => 'email',
       'age' => 'numeric|between:0,150',
-    ]);
+    ];
+    $messages = [
+      'name.required' => '名前は必ず入力してください。',
+      'mail.email' => 'メールアドレスが必要です。',
+      'age.numeric' => '年齢を整数で記入ください。',
+      'age.between' => '年齢は0～150の間で入力ください。',
+    ];
+    $validator = Validator::make($request->all(), $rules, $messages);
     if ($validator->fails()) {
       return redirect('/hello')
         ->withErrors($validator)
