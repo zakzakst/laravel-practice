4797e90fc2f04c74933458e2f63d548d64c0f8eb
zakzakst <e1433104@gmail.com>
2022-03-19 22:50:14 +0900

    リスト4-23

--- app/Http/Requests/HelloRequest.php
+++ app/Http/Requests/HelloRequest.php
@@ -33,4 +33,13 @@ class HelloRequest extends FormRequest
       'age' => 'numeric|between:0,150',
     ];
   }
+
+  public function messages() {
+    return [
+      'name.required' => '名前は必ず入力してください。',
+      'mail.email' => 'メールアドレスが必要です。',
+      'age.numeric' => '年齢を整数で記入ください。',
+      'age.between' => '年齢は0～150の間で入力ください。',
+    ];
+  }
 }
