5364bc5f5cb192ef4e0ce35373828a502c4f0b44
zakzakst <e1433104@gmail.com>
2022-03-19 22:55:43 +0900

    リスト4-24

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\HelloRequest;
+// use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
@@ -11,7 +12,17 @@ class HelloController extends Controller
     return view('hello.index', ['msg'=>'フォームを入力：']);
   }
 
-  public function post(HelloRequest $request) {
+  public function post(Request $request) {
+    $validator = Validator::make($request->all(), [
+      'name' => 'required',
+      'mail' => 'email',
+      'age' => 'numeric|between:0,150',
+    ]);
+    if ($validator->fails()) {
+      return redirect('/hello')
+        ->withErrors($validator)
+        ->withInput();
+    }
     return view('hello.index', ['msg'=>'正しく入力されました！']);
   }
 }
