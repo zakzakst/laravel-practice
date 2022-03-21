aa93a73b6a97e4b9dd4611080bc52a1259d92af1
zakzakst <e1433104@gmail.com>
2022-03-21 22:19:38 +0900

    リスト4-37,38

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,10 +9,23 @@ use Validator;
 class HelloController extends Controller
 {
   public function index(Request $request) {
-    return view('hello.index', ['msg' => 'フォームを入力ください。']);
+    if ($request->hasCookie('msg'))
+    {
+      $msg = 'Cookie: ' . $request->cookie('msg');
+    } else {
+      $msg = '※クッキーはありません。';
+    }
+    return view('hello.index', ['msg' => $msg]);
   }
 
-  public function post(HelloRequest $request) {
-    return view('hello.index', ['msg'=>'正しく入力されました！']);
+  public function post(Request $request) {
+    $validate_rule = [
+      'msg' => 'required',
+    ];
+    $this->validate($request, $validate_rule);
+    $msg = $request->msg;
+    $response = response()->view('hello.index', ['msg'=>'「' . $msg . '」をクッキーに保存しました。']);
+    $response->cookie('msg', $msg, 100);
+    return $response;
   }
 }
