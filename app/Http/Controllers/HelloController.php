eb965a348519d43d1e1bce1736c4af076d5af971
zakzakst <e1433104@gmail.com>
2022-03-24 01:48:51 +0900

    リスト5-7

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,7 +10,13 @@ use Illuminate\Support\Facades\DB;
 class HelloController extends Controller
 {
   public function index(Request $request) {
-    $items = DB::select('select * from people');
+    if (isset($request->id))
+    {
+      $param = ['id' => $request->id];
+      $items = DB::select('select * from people where id = :id', $param);
+    } else {
+      $items = DB::select('select * from people');
+    }
     return view('hello.index', ['items' => $items]);
   }
 
