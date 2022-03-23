daf2d3f8a1c7a39866c36b98a6117258d6e6c657
zakzakst <e1433104@gmail.com>
2022-03-24 01:30:42 +0900

    修正

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,17 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
 use Validator;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
   public function index(Request $request) {
-    if ($request->hasCookie('msg'))
-    {
-      $msg = 'Cookie: ' . $request->cookie('msg');
-    } else {
-      $msg = '※クッキーはありません。';
-    }
-    return view('hello.index', ['msg' => $msg]);
+    $items = DB::select('select * from people');
+    return view('hello.index', ['items' => $items]);
   }
 
   public function post(Request $request) {
--- resources/views/layouts/helloapp.blade.php
+++ resources/views/layouts/helloapp.blade.php
@@ -5,6 +5,8 @@
     body {font-size: 16pt; color: #999; margin: 5px;}
     h1 {font-size: 50pt; text-align: right; color: #f6f6f6; margin: -20px 0 -30px 0; letter-spacing: -4pt}
     ul {font-size: 12pt;}
+    th {background-color: #999; color: #fff; padding: 5px 10px;}
+    td {border: solid 1px #aaa; color: #999; padding: 5px 10px;}
     hr {margin: 25px 100px; border-top: 1px dashed #ddd;}
     .menutitle {font-size: 14pt; font-weight: bold; margin: 0;}
     .content {margin: 10px;}
