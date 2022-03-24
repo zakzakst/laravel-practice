803d04225ca0742c37a4a41edfc3b78bfd7c429c
zakzakst <e1433104@gmail.com>
2022-03-24 20:21:21 +0900

    リスト5-28

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -34,19 +34,21 @@ class HelloController extends Controller
   }
 
   public function edit(Request $request) {
-    $param = ['id' => $request->id];
-    $item = DB::select('select * from people where id = :id', $param);
-    return view('hello.edit', ['form' => $item[0]]);
+    $item = DB::table('people')
+      ->where('id', $request->id)
+      ->first();
+    return view('hello.edit', ['form' => $item]);
   }
 
   public function update(Request $request) {
     $param = [
-      'id' => $request->id,
       'name' => $request->name,
       'mail' => $request->mail,
       'age' => $request->age,
     ];
-    DB::update('update people set name = :name, mail = :mail, age = :age where id = :id', $param);
+    DB::table('people')
+      ->where('id', $request->id)
+      ->update($param);
     return redirect('/hello');
   }
 
