c1a733c26ef2c4edf8dc142f09853ea712c81e83
zakzakst <e1433104@gmail.com>
2022-03-24 20:17:49 +0900

    リスト5-27

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -29,7 +29,7 @@ class HelloController extends Controller
       'mail' => $request->mail,
       'age' => $request->age,
     ];
-    DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
+    DB::table('people')->insert($param);
     return redirect('/hello');
   }
 
