e62c3ff151dd854b668d1d666683b86346531507
zakzakst <e1433104@gmail.com>
2022-03-24 20:13:19 +0900

    リスト5-25

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -63,10 +63,10 @@ class HelloController extends Controller
   }
 
   public function show(Request $request) {
-    $name = $request->name;
+    $min = $request->min;
+    $max = $request->max;
     $items = DB::table('people')
-      ->where('name', 'like', '%' . $name . '%')
-      ->orWhere('mail', 'like', '%'. $name . '%')
+      ->whereRaw('age >= ? and age <= ?', [$min, $max])
       ->get();
     return view('hello.show', ['item' => $item]);
   }
