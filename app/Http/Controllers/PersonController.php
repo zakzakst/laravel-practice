7e3a9c50689b4d0e91fb692174c9736857d8307c
zakzakst <e1433104@gmail.com>
2022-03-26 01:38:33 +0900

    リスト6-10

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -17,8 +17,8 @@ class PersonController extends Controller
     }
 
     public function search(Request $request) {
-        $item = Person::find($request->input);
-        $param = ['input' => $request-> input, 'item' => $item];
+        $item = Person::where('name', $request->input)->first();
+        $param = ['input' => $request->input, 'item' => $item];
         return view('person.find', $param);
     }
 }
