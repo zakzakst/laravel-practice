0b96855bda057d8bcc116f595cf4738b648e9d38
zakzakst <e1433104@gmail.com>
2022-03-26 01:54:20 +0900

    リスト6-13,14

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -17,7 +17,11 @@ class PersonController extends Controller
     }
 
     public function search(Request $request) {
-        $item = Person::nameEqual($request->input)->first();
+        $min = $request->input * 1;
+        $max = $min + 10;
+        $item = Person::ageGreaterThan($min)
+            ->ageLessThan($max)
+            ->first();
         $param = ['input' => $request->input, 'item' => $item];
         return view('person.find', $param);
     }
--- app/Models/Person.php
+++ app/Models/Person.php
@@ -15,4 +15,12 @@ class Person extends Model
     public function scopeNameEqual($query, $str) {
         return $query->where('name', $str);
     }
+
+    public function scopeAgeGreaterThan($query, $n) {
+        return $query->where('age', '>=', $n);
+    }
+
+    public function scopeAgeLessThan($query, $n) {
+        return $query->where('age', '<=', $n);
+    }
 }
