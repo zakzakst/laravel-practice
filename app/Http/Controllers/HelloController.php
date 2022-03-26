6111a8fb899262401bb806fb035c2670485b2fc1
zakzakst <e1433104@gmail.com>
2022-03-26 21:25:35 +0900

    リスト7-24,25

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -12,11 +12,8 @@ class HelloController extends Controller
 {
   public function index(Request $request) {
     $sort = $request->sort;
-    // $items = DB::table('people')
-    //   ->orderBy($sort, 'asc')
-    //   ->simplePaginate(5);
     $items = Person::orderBy($sort, 'asc')
-      ->simplePaginate(5);
+      ->paginate(5);
     $param = [
       'items' => $items,
       'sort' => $sort,
