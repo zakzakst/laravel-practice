415584110e606df39425ecb21e2311defd8d16f2
zakzakst <e1433104@gmail.com>
2022-03-26 17:50:10 +0900

    リスト6-35,36

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -22,4 +22,8 @@ class Person extends Model
     public function getData() {
         return $this->id . ': ' . $this->name . '（' . $this->age . '）';
     }
+
+    public function board() {
+        return $this->hasOne('App\Board');
+    }
 }
--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -10,11 +10,17 @@
 @section('content')
   <table>
     <tr>
-      <th>Data</th>
+      <th>Person</th>
+      <th>Board</th>
     </tr>
     @foreach ($items as $item)
       <tr>
         <td>{{$item->getData()}}</td>
+        <td>
+          @if ($item->board != null)
+            {{$item->board->getData()}}
+          @endif
+        </td>
       </tr>
     @endforeach
   </table>
